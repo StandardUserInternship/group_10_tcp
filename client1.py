6401906d8698e2e3e37fa8a97f8f008e0006a5bd
Kelly <Kellyvypham@gmail.com>
2022-07-13 09:39:01 -0500

    Kelly

--- client1.py
+++ client1.py
@@ -4,30 +4,15 @@ import socket
 import sys
 from threading import*
 
-
-
-
 host= "127.0.0.1"
 port= 50007
-
-
-
-
 users=[]
-
-
-
 for i in users:
     user= input('Enter your user name:')
-
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
- 
-
- s.connect((host, port))
-
- s.sendall(b' Test Message ')
- 
- data = s.recv(1024)
+    s.connect((host, port))
+    s.sendall(b' Test Message ')
+    data = s.recv(1024)
 print('Other Message', repr(data))
     
     
